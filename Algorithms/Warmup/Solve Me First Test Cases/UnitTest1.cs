e13310b3338de744b2e76484fd3ee129db8929d6
quincy <quincyrchen@ucla.edu>
2018-02-06 18:26:16 -0800

    Added the Solve Me First solution and test case!

---
+++ Algorithms/Warmup/Solve Me First Test Cases/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solve_Me_First;
+
+namespace Solve_Me_First_Test_Cases
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void simpleAddition()
+        {
+            int a = 1;
+            int b = 1;
+            int expected = 2;
+            // 1+1 = 2; This should be relatively self-evident.
+
+            int actual = Solution.solveMeFirst(a, b);
+
+            Assert.AreEqual(expected, actual, 0.001, "Test case Failed");
+        }
+    }
+}
---
+++ Algorithms/Warmup/Solve Me First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve_Me_First
+{
+    public class Solution
+    {
+        public static int solveMeFirst(int a, int b)
+        {
+            //We're just adding two integers together here. Hopefully you don't have any trouble with this trial question!
+            return a + b;
+
+        }
+        static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
