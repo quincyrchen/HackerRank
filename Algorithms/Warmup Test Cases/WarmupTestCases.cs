b49ce07cac21d51c1c9c408ae15bc33cc7d1191c
quincy <quincyrchen@ucla.edu>
2018-02-07 10:10:11 -0800

    Restructured project filesystem

---
+++ Algorithms/Warmup Test Cases/WarmupTestCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolveMeFirst_TestCases
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            int x = 1;
+            int y = 2;
+            int expected = 3;
+            int actual = SolveMeFirst.Solution.solveMeFirst(x, y);
+
+            Assert.AreEqual(expected, actual, "AddTwoNumbers Test Case Failed.");
+        }
+    }
+}
