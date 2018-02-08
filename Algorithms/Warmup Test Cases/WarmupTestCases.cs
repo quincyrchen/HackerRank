19db3a8665e28eda2a5c72e1e0fda25edd082d5c
quincy <quincyrchen@ucla.edu>
2018-02-08 00:20:31 -0800

    Added simpleArraySum in Warmup

--- Algorithms/Warmup Test Cases/WarmupTestCases.cs
+++ Algorithms/Warmup Test Cases/WarmupTestCases.cs
@@ -7,14 +7,29 @@ namespace SolveMeFirst_TestCases
     public class UnitTests
     {
         [TestMethod]
-        public void AddTwoNumbers()
+        public void solveMeFirst()
         {
             int x = 1;
             int y = 2;
             int expected = 3;
             int actual = SolveMeFirst.Solution.solveMeFirst(x, y);
+            Assert.AreEqual(expected, actual, "solveMeFirst Test Case Failed.");
+        }
+    }
+}
 
-            Assert.AreEqual(expected, actual, "AddTwoNumbers Test Case Failed.");
+namespace SimpleArraySum_TestCases
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void simpleArraySum() {
+            int[] ar = { 1, 2, 3, 4, 5 };
+            int n = 5;
+            int expected = 15;
+            int actual = SimpleArraySum.Solution.simpleArraySum(n, ar);
+            Assert.AreEqual(expected, actual, "simpleArraySum Test Case Failed.")
         }
     }
 }
--- Algorithms/Warmup/Warmup.cs
+++ Algorithms/Warmup/Warmup.cs
@@ -14,6 +14,7 @@ namespace SolveMeFirst
             //We are just adding two numbers here; I hope you don't have any issues!
             return a + b;
         }
+
         static void Main(String[] args)
         {
             int val1 = Convert.ToInt32(Console.ReadLine());
@@ -23,3 +24,32 @@ namespace SolveMeFirst
         }
     }
 }
+
+namespace SimpleArraySum
+{
+    public class Solution
+    {
+        public static int simpleArraySum(int n, int[] ar)
+        {
+            // Complete this function
+            //we declare the sum variable outside of the for loop to ensure that we don't reset it on each iteration
+            var sum = 0;
+            //we then iterate through the array and add the numbers up!
+            for (var i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+            return sum;
+            //Notice that n (the number of items in the array) is not used, since arrays have the Length property we can call on.
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = simpleArraySum(n, ar);
+            Console.WriteLine(result);
+        }
+
+    }
